936dea02f5beb17789c0d0d6d271b3593de7cb92
Glomzzz <glom@skillw.com>
2025-05-13 03:41:04 +0800

    init blink-cmp

--- config/plugins/cmp/blink-cmp.nix
+++ config/plugins/cmp/blink-cmp.nix
@@ -1,58 +1,12 @@
-{ helpers, ... }:
+{ pkgs-unstable,helpers, ... }:
 {
   extraPlugins = with pkgs-unstable.vimPlugins; [
     colorful-menu-nvim
   ];
   plugins.blink-cmp = {
       enable = true;
-      settings = {
-        completion = {
-          accept = {
-            auto_brackets = {
-              enabled = true;
-              semantic_token_resolution = {
-                enabled = true;
-              };
-            };
-          };
-          menu = {
-            auto_show = true;
-            draw = helpers.mkRaw ''
-              {
-              columns = { { 'kind_icon' }, { 'label', gap = 1 } },
-              components = {
-                label = {
-                  text = function(ctx)
-                    return require('colorful-menu').blink_components_text(ctx)
-                  end,
-                  highlight = function(ctx)
-                    return require('colorful-menu').blink_components_highlight(ctx)
-                  end,
-                },
-              }
-            '';
-          };
-        };
-        signature = {
-          enabled = true;
-        };
-        cmdline = {
-          completion = {
-            menu = {
-              auto_show = true;
-            };
-          };
-        };
-        sources = {
-          cmdline = [ ];
-          providers = {
-            snippets.score_offset = 1000;
-            buffer.score_offset = -7;
-            lsp = {
-              fallbacks = [ ];
-            };
-          };
-        };
-      };
+      
   };
+
+  extraConfigLua = builtins.readFile (./blink-cmp.lua);
 }
