36725bf4954926394953fa5b5e243684e314c733
Glomzzz <glom@skillw.com>
2025-05-15 00:30:45 +0800

    add playground for nixvim

---
+++ playground/hello.py
@@ -0,0 +1,28 @@
+
+
+a = 100
+b = 200
+
+
+def print_a():
+    print(a)
+
+
+def print_a_b():
+    print(a)
+    print(b)
+
+def error():
+    # fix_me
+    print(c)
+
+def with_param(a,b,c):
+    print(a)
+    print(d) # fix_me
+
+
+
+
+
+
+
