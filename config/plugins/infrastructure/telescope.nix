762d831d3dcd15f7b5b9e704e9a0813d7b9481c0
Glomzzz <glom@skillw.com>
2025-05-21 05:29:53 +0800

    enhance telesope: vertical view and find/live_grep hidden files

--- config/plugins/infrastructure/telescope.nix
+++ config/plugins/infrastructure/telescope.nix
@@ -1,17 +1,50 @@
-{ helpers, ... }:
-
-{
+{helpers, ...}: {
   plugins.telescope = {
     enable = true;
+    extensions = {
+      live-grep-args.enable = true;
+      fzf-native = {
+        enable = true;
+        settings = {
+          fuzzy = true;
+          override_generic_sorter = true;
+          override_file_sorter = true;
+          case_mode = "smart_case";
+        };
+      };
+      ui-select = {
+        enable = true;
+        settings = {
+          specific_opts = {
+            codeactions = true;
+          };
+        };
+      };
+    };
+    settings = {
+      defaults = {
+        layout_strategy = "vertical";
+        layout_config = {
+          vertical = {
+            width = 0.9;
+            height = 0.9;
+            preview_height = 0.6;
+            preview_cutoff = 0;
+          };
+        };
+      };
+    };
   };
 
   keymaps = [
     {
-      mode = [ "n" ];
+      mode = ["n"];
       key = "<leader>/";
       action = helpers.mkRaw ''
         function()
-          require("telescope.builtin").live_grep()
+          require("telescope").extensions.live_grep_args.live_grep_args{
+             additional_args = function(args) return vim.list_extend(args, { "--hidden" }) end,
+          }
         end
       '';
       options = {
@@ -19,11 +52,11 @@
       };
     }
     {
-      mode = [ "n" ];
+      mode = ["n"];
       key = "<leader><space>";
       action = helpers.mkRaw ''
         function()
-          require("telescope.builtin").find_files()
+          require("telescope.builtin").find_files({hidden = true,})
         end
       '';
       options = {
